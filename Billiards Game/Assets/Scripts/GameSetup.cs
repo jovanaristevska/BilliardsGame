e1f4e9db6ec3531e880f2d1ed809c7ddd75ead50
jovanaristevska <ristevskaj@hotmail.com>
2025-02-12 02:44:37 +0100

    Tops added

--- Billiards Game/Assets/Scripts/GameSetup.cs
+++ Billiards Game/Assets/Scripts/GameSetup.cs
@@ -14,11 +14,16 @@ public class GameSetup : MonoBehaviour
     [SerializeField] Transform cueBallPosition;
     [SerializeField] Transform headBallPosition;
 
+    //
+    private List<GameObject> balls = new List<GameObject>();
+
     void Start()
     {
-        ballRadius = ballPrefab.GetComponent<SphereCollider>().radius * 100f;
+        ballRadius = ballPrefab.GetComponent<SphereCollider>().radius * 150f;
         ballDiametar = ballRadius * 2f;
         PlaceAllBalls();
+        ////
+        //DisableBallPhysics(); // Disable physics initially
     }
 
     void PlaceAllBalls()
@@ -31,12 +36,16 @@ public class GameSetup : MonoBehaviour
     {
         GameObject ball = Instantiate(ballPrefab, cueBallPosition.position, Quaternion.identity);
         ball.GetComponent<Ball>().MakeCueBall();
+        ////
+        //balls.Add(ball); // Add the ball to the list
     }
 
     void PlaceEightBall(Vector3 position)
     {
         GameObject ball = Instantiate(ballPrefab, position, Quaternion.identity);
         ball.GetComponent<Ball>().MakeEightBall();
+        ////
+        //balls.Add(ball); // Add the ball to the list
     }
 
     void PlaceRandomBalls()
@@ -46,11 +55,18 @@ public class GameSetup : MonoBehaviour
         Vector3 firstInRowPosition = headBallPosition.position;
         Vector3 currentPosition = firstInRowPosition;
 
+        ////
+        //// Add a small spacing offset (e.g., 5% of the ball diameter)
+        //float spacingOffset = ballDiametar * 0.05f; // Adjust this value to control the spacing
+        //float totalSpacing = ballDiametar + spacingOffset;
+
         void PlaceRedBall(Vector3 position)
         {
             GameObject ball = Instantiate(ballPrefab, position, Quaternion.identity);
             ball.GetComponent<Ball>().BallSetup(true);
             redBallsRemaining--;
+            ////
+            //balls.Add(ball); // Add the ball to the list
         }
 
         void PlaceYellowBall(Vector3 position)
@@ -58,6 +74,8 @@ public class GameSetup : MonoBehaviour
             GameObject ball = Instantiate(ballPrefab, position, Quaternion.identity);
             ball.GetComponent<Ball>().BallSetup(false);
             yellowBallsRemaining--;
+            ////
+            //balls.Add(ball); // Add the ball to the list
         }
 
         for (int i = 0; i < 5; i++)
@@ -94,17 +112,44 @@ public class GameSetup : MonoBehaviour
                 }
 
                 //Move the current position for the next ball in tis row to the right
+                //currentPosition += new Vector3(totalSpacing, 0, 0); // No need to normalize here
                 currentPosition += new Vector3(1, 0, 0).normalized * ballDiametar;
 
             }
 
-            //Once all the balls in the row have been placed, move to the next row
-            firstInRowPosition += Vector3.back*(Mathf.Sqrt(3) * ballRadius) + Vector3.left * ballRadius; ;
+            // Once all the balls in the row have been placed, move to the next row
+            //firstInRowPosition += Vector3.back * (Mathf.Sqrt(3) / 2f * totalSpacing); // Vertical spacing
+            //firstInRowPosition += Vector3.left * (totalSpacing / 2f); // Offset for the next row
+            firstInRowPosition += Vector3.back * (Mathf.Sqrt(3) * ballRadius) + Vector3.left * ballRadius;
             currentPosition = firstInRowPosition;
             NumInThisRow++;
 
         }
 
     }
+
+    //void DisableBallPhysics()
+    //{
+    //    foreach (var ball in balls)
+    //    {
+    //        Rigidbody rb = ball.GetComponent<Rigidbody>();
+    //        if (rb != null)
+    //        {
+    //            rb.isKinematic = true; // Disable physics
+    //        }
+    //    }
+    //}
+
+    //public void EnableBallPhysics()
+    //{
+    //    foreach (var ball in balls)
+    //    {
+    //        Rigidbody rb = ball.GetComponent<Rigidbody>();
+    //        if (rb != null)
+    //        {
+    //            rb.isKinematic = false; // Enable physics
+    //        }
+    //    }
+    //}
 }
 
